Minor change to fix gradient descent example

Week5_SupervisedMachineLearning_Examples/GradientDescent.py
@@ -1,60 +1,62 @@
 import numpy as np
 import pandas as pd
+import os
+import sys
 import matplotlib.pyplot as plt
 
 x_prime = []
 
-def batch_gradient_descent1(y, X, alpha, epsilon, 
+def batch_gradient_descent1(y, X, alpha, epsilon,
                            plot_on=-1, plot_func=None):
     # Initialize our "guess" for each coefficient theta_j to 1
     # and store these in a single column
     theta = np.ones(shape=(X.shape[1], 1))
-    
+
     m = X.shape[0] # number of data points
-    
+
     # Calculate a column of predicted y values for each data point
     y_hat = X @ theta
-    
+
     # calculate a 1 by 1 matrix that holds the sum of the squared
     # differences between each y_hat and y
     cost = np.transpose(y_hat - y) @ (y_hat - y)
-    
+
     # initialize list of costs to contain the cost associated with
     # our initial coefficients (scaled by 1/2m in accordance with
     # the cost formula)
     costs = [cost[0][0] / (2 * m)]
-    
+
     i = 0 # number of iterations
     delta = 1 # Change in cost
-    
+
     while (delta > epsilon):
         if (plot_on > 0 and i % plot_on == 0 and plot_func is not None):
             plot_func(theta)
-        
+
         # calculate a column that holds the difference between y_hat and
         # y for each data point
         differences = X @ theta - y
-        
+
         # Update each theta_j by the partial derivative of the cost with
         # respect to theta_j, scaled by learning rate
-        
+
         # Note: np.transpose(X) gives us the observed values (x_j) for
         # a parameter j in the j'th row of a matrix
         theta = theta - (alpha / m) * ((np.transpose(X)) @ differences)
-        
+
         # Using the updated coefficient values, append the new cost value
         cost = np.transpose(X @ theta - y) @ (X @ theta - y)
         costs.append(cost[0][0] / (2 * m))
         delta = abs(costs[i + 1] - costs[i])
-        
+
         if (costs[i + 1] > costs[i]):
             print('Cost is increasing. Try reducing alpha.')
             break
         i += 1
-    
+
     if (plot_on > 0 and i % plot_on == 0 and plot_func is not None):
         plot_func(theta)
-        
+
     print('Completed in', i, 'iterations.')
     return theta
 
@@ -70,29 +72,32 @@ def batch_gradient_descent2(y, X, alpha, epsilon):
     m = X.shape[0]
     cost = np.transpose(X @ theta - y) @ (X @ theta - y)
     costs = [cost[0][0] / (2 * m)]
-    
+
     i = 0
     delta = 1
-    
+
     while (delta > epsilon):
         theta = theta - (alpha / m) * ((np.transpose(X)) @ (X @ theta - y))
-                
+
         cost = np.transpose(X @ theta - y) @ (X @ theta - y)
         costs.append(cost[0][0] / (2 * m))
         delta = abs(costs[i + 1] - costs[i])
-        
+
         if (costs[i + 1] > costs[i]):
             print('Cost is increasing. Try reducing alpha.')
             break
         i += 1
-        
+
     print('Completed in', i, 'iterations.')
     # Plot the cost versus iteration
     plt.plot([i for i in range(len(costs))], costs)
     return theta
 
 def main():
-    df = pd.read_csv('regression.csv')
+    # Full filename to avoid issues opening file
+    input_dataset_file = os.path.join(sys.path[0], "regression.csv")
+
+    df = pd.read_csv(input_dataset_file)
     X = df[['x']]
     y = df[['y']]
     plt.xlabel('x')
@@ -115,7 +120,7 @@ def main():
     plt.show()
 
     # Set up our predictor / response columns
-    df = pd.read_csv('regression.csv')
+    df = pd.read_csv(input_dataset_file)
     X = df[['x']].to_numpy()
     y = df[['y']].to_numpy()
 