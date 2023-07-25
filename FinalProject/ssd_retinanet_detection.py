80d2c24e8064adecb4579411b6878212d3309070
davidoises <davidoises@hotmail.com>
2023-07-25 10:21:56 -0700

    Added option to run yolo from same script

--- FinalProject/ssd_retinanet_detection.py
+++ FinalProject/ssd_retinanet_detection.py
@@ -6,10 +6,15 @@ import cvzone
 import os
 import sys
 
+YOLOV5 = 0
 
-# model = torchvision.models.detection.ssd300_vgg16(pretrained = True)
-model = torchvision.models.detection.retinanet_resnet50_fpn(pretrained = True)
-model.eval()
+
+if YOLOV5:
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+else:
+    model = torchvision.models.detection.ssd300_vgg16(pretrained = True)
+    # model = torchvision.models.detection.retinanet_resnet50_fpn(pretrained = True)
+    model.eval()
 
 classes_file = os.path.join(sys.path[0], "classes.txt")
 classnames = []
@@ -19,27 +24,41 @@ with open(classes_file,'r') as f:
 # print(classnames[0])
 
 image_filename = os.path.join(sys.path[0], "pedestrians.jpg")
-image = cv2.imread(image_filename)
-img = image.copy()
-print(type(image))
 
-imgtransform = T.ToTensor()
-image = imgtransform(image)
-print(type(image))
+if YOLOV5:
 
-with torch.no_grad():
-    ypred = model([image])
-    print(ypred[0].keys())
+    # Inference
+    results = model(image_filename)
 
-    bbox,scores,labels = ypred[0]['boxes'],ypred[0]['scores'],ypred[0]['labels']
-    nums = torch.argwhere(scores > 0.30).shape[0]
-    for i in range(nums):
-        x,y,w,h = bbox[i].numpy().astype('int')
-        cv2.rectangle(img,(x,y),(w,h),(0,0,255),3)
-        classname = labels[i].numpy().astype('int')
-        classdetected = classnames[classname-1]
-        text = classdetected + " " + str(scores[i].numpy())
-        cvzone.putTextRect(img,text,[x,y+100],scale=1,border=1,thickness=2)
+    # Results
+    results.print()
+    results.save()  # or .show()
 
-cv2.imshow('frame',img)
-cv2.waitKey(0)
+    results.xyxy[0]  # img1 predictions (tensor)
+    rs = results.pandas().xyxy[0]  # img1 predictions (pandas)
+    print(rs)
+else:
+    image = cv2.imread(image_filename)
+    img = image.copy()
+    print(type(image))
+
+    imgtransform = T.ToTensor()
+    image = imgtransform(image)
+    print(type(image))
+
+    with torch.no_grad():
+        ypred = model([image])
+        print(ypred[0].keys())
+
+        bbox,scores,labels = ypred[0]['boxes'],ypred[0]['scores'],ypred[0]['labels']
+        nums = torch.argwhere(scores > 0.30).shape[0]
+        for i in range(nums):
+            x,y,w,h = bbox[i].numpy().astype('int')
+            cv2.rectangle(img,(x,y),(w,h),(0,0,255),3)
+            classname = labels[i].numpy().astype('int')
+            classdetected = classnames[classname-1]
+            text = classdetected + " " + str(scores[i].numpy())
+            cvzone.putTextRect(img,text,[x,y+100],scale=1,border=1,thickness=2)
+
+    cv2.imshow('frame',img)
+    cv2.waitKey(0)
